345a096469b6493b7992fd234c984873c066ce38
Faxriyor7 <faxriyorazimboyev3@gmail.com>
2024-12-01 21:42:36 +0500

    creat

---
+++ function_14_class/main.py
@@ -0,0 +1,71 @@
+# print("Hello World")
+#
+#
+# print("Hello Guys")
+#
+#
+# print("Hello Boys")
+#
+#
+# print("Hello Girls")
+
+# func()
+
+# def func():
+#     print("Hello World")
+
+# # func()
+# son1 = int(input("Sonni kiriting ::: "))
+# son2 = int(input("Sonni kiriting ::: "))
+#
+#
+# def summa(a, b):
+#     print(a + b)
+#
+#
+# summa(son1, son2)
+
+# def summa(a, *args):
+#     print(sum(args))
+#
+#
+# summa(7, 6, 6, 5, 5, 4, 3, 2)
+# summa(34, 12, 4, 4, 23)
+
+# meva1 = str(input("Digite o primeiro valor: "))
+# meva2 = str(input("Digite o primeiro valor: "))
+
+
+# def fruits(fruit1=None, fruit2=None):
+#     print(f"Men mevalardan {fruit1} va {fruit2}ni yaxshi ko'raman")
+#
+#
+# fruits(meva1)
+
+
+# def func(a, b, *args, **kwargs):
+#     print(a, b, args, kwargs)
+#
+#
+# func(1, 2, 3, "salom", 5, 6, name="dilshod", lastname="murtazoyev")
+
+
+# data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#
+#
+# def func(a):
+#     print(a)
+#
+# func(data)
+
+
+# def summa(a, b):
+#     return a + b, 123
+#
+#
+# natija = summa(1, 2)
+#
+# print(natija)
+
+
+
